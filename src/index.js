49f4588a5a49486f12da7520298a318337b44971
Vinicius Moraes <viniciusom01@hotmail.com>
2022-03-20 01:56:03 -0300

    initial express setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {return res.json('API running')});
+
+app.listen(3001, () => {console.log('Listening on port 3001')});
