d9faf7692bee61c1b2c4bfc10d9fcdb0ab6cf055
Vinicius Moraes <viniciusom01@hotmail.com>
2022-04-24 01:09:45 -0300

    feat: sequelize setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
+const db = require("./models");
 
-const authController = require('./controllers/auth');
+const authController = require("./controllers/auth");
 
 const app = express();
 
-app.use('/auth', authController);
+app.use("/auth", authController);
 
-app.get('/', (req, res) => {return res.json('API running')});
+app.get("/", (req, res) => {
+  return res.json("API running");
+});
 
-app.listen(3001, () => {console.log('Listening on port 3001')});
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Listening on port 3001");
+  });
+});
